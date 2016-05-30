17310264b22d171a4eac33754729d24c3c9762c2
Fabien Foerster <fabienfoerster@gmail.com>
2016-05-30 17:35:49 +0200

    change to match the true oauth2 flow

--- README.md
+++ README.md
@@ -4,19 +4,21 @@
 ## Utilisation
 
 ```go
-  conf := &oauth2.Config{
-    ClientID:     "xxx",
-    ClientSecret: "xxx",
-    Endpoint:     Endpoint,
-  }
+conf := &oauth2.Config{
+    ClientID:     "YOUR_CLIENT_ID",
+    ClientSecret: "YOUR_CLIENT_SECRET",
+    Scopes:       []string{"SCOPE1", "SCOPE2"},
+    Endpoint: oauth2.Endpoint{
+        AuthURL:  "https://provider.com/o/oauth2/auth",
+        TokenURL: "https://provider.com/o/oauth2/token",
+    },
+}
 
-  client := oauth2client.NewClient(*conf)
-  client.RetrieveCode()
-  fmt.Println(client.Code)
-  oauth2.RegisterBrokenAuthHeaderProvider(Endpoint.TokenURL)
-  tok, err := client.RetrieveToken()
-  if err != nil {
-    fmt.Printf("Fuck, it went wrong : %s\n", err)
-  }
-  fmt.Printf("And the token is : %s\n", tok.AccessToken)
+client := oauth2Client.NewClient(conf)
+code := client.RetrieveCode()
+
+tok, err := conf.Exchange(oauth2.NoContext, code)
+if err != nil {
+    log.Fatal(err)
+}
   ```
--- oauth2client.go
+++ oauth2client.go
@@ -13,7 +13,6 @@ type Oauth2Client struct {
 	codeChan chan string // channel use to retrieve the code from the dummy server
 	Conf     *oauth2.Config
 	port     int
-	Code     string
 }
 
 func (o *Oauth2Client) handleCode(w http.ResponseWriter, r *http.Request) {
@@ -27,27 +26,22 @@ func (o *Oauth2Client) setPort(port int) {
 	o.Conf.RedirectURL = fmt.Sprintf("http://localhost:%d", port)
 }
 
-func NewClient(conf oauth2.Config) *Oauth2Client {
+func NewClient(conf *oauth2.Config) *Oauth2Client {
 	client := &Oauth2Client{
 		codeChan: make(chan string),
-		Conf:     &conf,
+		Conf:     conf,
 		port:     3000,
 	}
 	client.Conf.RedirectURL = fmt.Sprintf("http://localhost:%d", client.port)
 	return client
 }
 
-func (o *Oauth2Client) RetrieveCode() {
+func (o *Oauth2Client) RetrieveCode() string {
 	http.HandleFunc("/", o.handleCode)
 	port := fmt.Sprintf(":%d", o.port)
 	go http.ListenAndServe(port, nil)
 	url := o.Conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
 	browser.OpenURL(url)
-	o.Code = <-o.codeChan
-}
-
-// RetrieveToken is an exported function
-func (o *Oauth2Client) RetrieveToken() (oauth2.Token, error) {
-	tok, err := o.Conf.Exchange(oauth2.NoContext, o.Code)
-	return *tok, err
+	code := <-o.codeChan
+	return code
 }
